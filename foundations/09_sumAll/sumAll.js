c55b975121c511d9b2f936f84c978f1df20b5cef
KailasMetcalfe <kailasmetz@gmail.com>
2026-06-26 16:09:16 +0100

    Finished

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0) return "ERROR";
+    else if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+
+    const smaller = Math.min(a, b);
+    const larger = Math.max(a, b);
+    let sum = 0;
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
