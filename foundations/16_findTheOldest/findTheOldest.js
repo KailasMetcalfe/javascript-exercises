7aa0c8346d7425f700a185438ceac22489c7e726
KailasMetcalfe <kailasmetz@gmail.com>
2026-06-30 15:39:21 +0100

    Finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, curr) => {
+        const currentAge = ("yearOfDeath" in curr) ? 
+            curr.yearOfDeath - curr.yearOfBirth : new Date().getFullYear() - curr.yearOfBirth;
+        
+        const oldestAge = ("yearOfDeath" in oldest) ? 
+            oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
 
+        if(oldestAge < currentAge) return curr;
+        else return oldest;
+    })
 };
 
 // Do not edit below this line
